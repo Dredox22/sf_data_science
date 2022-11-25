d347da8bdd37a17dba8c956374327abbf883b4e6
Dredox22 <116824141+Dredox22@users.noreply.github.com>
2022-11-25 23:08:16 +0300

    edit profit_company.ipynb

--- guess-number-task/1.py
+++ guess-number-task/1.py
@@ -1,3 +1,15 @@
+import numpy as np
 print(np.sctypeDict)
 print(len(np.sctypeDict))
 # 158, но может быть 135 или 139
+import pandas as pd
+print(pd.__name__)
+print (pd.__version__)
+
+import pandas as pd
+countries = pd.Series(
+    data = ['Англия', 'Канада', 'США', 'Россия', 'Украина', 'Беларусь', 'Казахстан'],
+    index = ['UK', 'CA', 'US', 'RU', 'UA', 'BY', 'KZ'],
+    name = 'countries'
+)
+print(countries)
